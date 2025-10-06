ae92ebd8f944cc6556fa094eec8a4abe39097723
Nicholas Steicke <67732+narthollis@users.noreply.github.com>
2025-10-06 20:15:12 +1030

    update talos and kube

--- update-talhelper.sh
+++ update-talhelper.sh
@@ -4,7 +4,7 @@ mkdir -p /tmp/talhelperdl
 pushd /tmp/talhelperdl
 
 curl -L https://github.com/budimanjojo/talhelper/releases/latest/download/talhelper_linux_amd64.tar.gz -o talhelper.tar.gz
-tar xvzf /tmp/talhelper.tar.gz talhelper
+tar xvzf talhelper.tar.gz talhelper
 
 mv talhelper "$(which talhelper)"
 
